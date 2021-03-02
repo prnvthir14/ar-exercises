b36626a1af51597f30a833365acb1506b9eee5a8
prnvthir14 <prnvthir@gmail.com>
2021-03-02 19:18:08 +0000

    complete exercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,9 +7,9 @@ puts "----------"
 # class Store < ApplicationRecord
 # end
 
-store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel:true, womens_apparel:true)
-store2 = Store.create(name:"Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-store3 = Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+@store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel:true, womens_apparel:true)
+@store2 = Store.create(name:"Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+@store3 = Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 countOfStores = puts Store.count; 
 
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -7,5 +7,18 @@ require_relative './exercise_5'
 
 puts "Exercise 6"
 puts "----------"
+puts "STORE 4 -------- #{@store4}"
 
 # Your code goes here ...
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 160)
+@store1.employees.create(first_name: "Khurra", last_name: "Virani", hourly_rate: 260)
+@store1.employees.create(first_name: "Khurr", last_name: "Virani", hourly_rate: 360)
+@store2.employees.create(first_name: "Khur", last_name: "Virani", hourly_rate: 460)
+@store2.employees.create(first_name: "Khu", last_name: "Virani", hourly_rate: 560)
+@store2.employees.create(first_name: "Kh", last_name: "Virani", hourly_rate: 660)
+@store4.employees.create(first_name: "K", last_name: "Virani", hourly_rate: 760)
+@store5.employees.create(first_name: "Khuraaram", last_name: "Viran", hourly_rate: 60)
+@store6.employees.create(first_name: "Khurrssam", last_name: "Vira", hourly_rate: 60)
+@store4.employees.create(first_name: "Khurqqram", last_name: "Vir", hourly_rate: 60)
